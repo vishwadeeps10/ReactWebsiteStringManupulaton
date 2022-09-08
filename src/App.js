a99f0ed7e8ae42c930ad603e5f0fe913cb598d43
Vishwadeep <vs160698@gmail.com>
2022-09-08 11:40:15 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from "react";
+
 
 function App() {
+  const [mode ,setMode] = useState('light');
+ 
+const toggleMode=()=>{
+  if(mode==='light'){
+    setMode('dark')
+    document.body.style.backgroundColor='rgb(48 69 90)';
+    document.body.style.color='white';
+  }
+  else{
+    setMode('light')
+    document.body.style.backgroundColor='white';
+    document.body.style.color='black';
+  }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<Navbar title="TextTilled" mode={mode} toggleMode={toggleMode}/>
+<TextForm mode={mode}/>
+{/* <About/> */}
+    </>
   );
 }
 
