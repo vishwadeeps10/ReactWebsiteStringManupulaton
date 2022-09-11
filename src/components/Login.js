8bff72fa056de2f8dd1953440b24020ba168bcc3
Vishwadeep Singh <vishwadeeps10@gmail.com>
2022-09-11 12:40:50 +0530

    fix bug in css

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,17 +6,17 @@ export default function Login() {
    
   return (
   
-    <div className='container'  >
+    <div className='container' id="containerId"  >
         <h2 className='mb-3'>Login Here</h2>
         <form>
             <div className="form-group">
                 <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                <input type="email" className="form-control" id="loginInputEmail" aria-describedby="emailHelp" placeholder="Enter email" />
             </div>
 
             <div className="form-group">
                 <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Enter Password" />
+                <input type="password" className="form-control" id="loginInputPassword" placeholder="Enter Password" />
             </div>
 
             <button type="submit" id='buttonId' className="btn btn-primary btn-block mt-2 mb-2">Login</button>
